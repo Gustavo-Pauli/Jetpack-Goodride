08459f4e88da29a3bb2d5f2bd2722491894d812c
Gustavo Pauli <gustavo.pauli0@gmail.com>
2021-08-21 03:38:52 -0300

    First commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+<h1>Jetpack Joyride clone</h1>
+
+Made using pygame.
+
+This game was made for **learning purpose only**.
---
+++ settings.py
@@ -0,0 +1,17 @@
+# game settings
+TITLE = 'Jetpack Happyride'
+ICON_LOC = 'assets/sprites/icon.png'
+DEFAULT_FONT_LOC = 'assets/fonts/New Athletic M54.ttf'
+HIGH_SCORE_LOC = 'save/highscore.txt'
+HIGH_SCORE = 0
+WIDTH = 1280
+HEIGHT = 720
+FPS = 150
+
+# player settings
+DEFAULT_VELOCITY_X = 420
+GRAVITY = 1.5
+
+# scenario settings
+OBSTACLE_SPAWN_TIME = 1300
+OBSTACLE_OFFSET = 2000
